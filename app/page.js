b2ce3a724dcb76949e298397f782ddfb8e6cc6bc
Utkarsh Singh Rawat <utkarsh.srawat2003@gmail.com>
2025-12-13 19:58:31 +0530

    header logo

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
-    <div className="h-screen max-w-screen bg-amber-900 flex items-center justify-center">
-     hi
+    <div className="">
     </div>
   );
 }
---
+++ components/header.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="fixed top-0 right-0 left-0 bg-background/80 backdrop-blur-xl z-20 border-b">
+        <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+          {/* Logo */}
+          <Link href={'/'} className="flex items-center justify-center">
+            <Image
+              src="/spott.png"
+              alt="Spott logo"
+              height={500}
+              width={500}
+              className="w-full h-11"
+              priority
+            />
+            {/* pro tag */}
+          </Link>
+          {/* Search & Location */}
+          {/* Right side actions */}
+        </div>
+        {/* Mobile Search location */}
+      </nav>
+      {/* Modals */}
+    </>
+  );
+};
+
+export default Header;
