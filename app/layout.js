8d065d6d51e9518e80ba1698cd5879f908bdc28d
Utkarsh Singh Rawat <utkarsh.srawat2003@gmail.com>
2025-12-14 09:11:59 +0530

    Boiler plate ConvexClientProvider

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Header from "@/components/header";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
+import { ConvexClientProvider } from "./ConvexClientProvider";
 export const metadata = {
   title: "Spott",
   description: "Discover and create amazing events",
@@ -18,19 +19,21 @@ export default function RootLayout({ children }) {
           enableSystem
           disableTransitionOnChange
         >
-          <Header/>
-          <main className="relative min-h-screen container mx-auto pt-40 md:pt-32">
-            <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
-              <div className="absolute bottom-0 left-1/32 w-96 h-96 bg-red-600/20 rounded-full blur-3xl"/>
-              <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl"/>
-              <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl"/>
-              <div className="absolute top-0 right-1/32 w-96 h-96 bg-green-600/20 rounded-full blur-3xl"/>
-            </div>
-            <div className="relative z-10 min-h-[70vh]">{children}</div>
-            <footer className="border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-auto">
-              <div className="text-sm text-gray-400">Made by Utkarsh</div>
-            </footer>
-          </main>
+          <ConvexClientProvider>
+            <Header />
+            <main className="relative min-h-screen container mx-auto pt-40 md:pt-32">
+              <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
+                <div className="absolute bottom-0 left-1/32 w-96 h-96 bg-red-600/20 rounded-full blur-3xl" />
+                <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl" />
+                <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl" />
+                <div className="absolute top-0 right-1/32 w-96 h-96 bg-green-600/20 rounded-full blur-3xl" />
+              </div>
+              <div className="relative z-10 min-h-[70vh]">{children}</div>
+              <footer className="border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-auto">
+                <div className="text-sm text-gray-400">Made by Utkarsh</div>
+              </footer>
+            </main>
+          </ConvexClientProvider>
         </ThemeProvider>
       </body>
     </html>
--- components/header.jsx
+++ components/header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
       <nav className="fixed top-0 right-0 left-0 bg-background/80 backdrop-blur-xl z-20 border-b">
         <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
           {/* Logo */}
-          <Link href={'/'} className="flex items-center justify-center">
+          <Link href={"/"} className="flex items-center justify-center">
             <Image
               src="/spott.png"
               alt="Spott logo"
