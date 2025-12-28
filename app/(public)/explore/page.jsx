903c64a5ef823a258080193549b2e62d1e43b7af
Utkarsh Singh Rawat <utkarsh.srawat2003@gmail.com>
2025-12-28 18:20:42 +0530

    convex hooks

--- app/(public)/explore/page.jsx
+++ app/(public)/explore/page.jsx
@@ -1,10 +1,10 @@
 "use client"
 import { api } from "@/convex/_generated/api";
-import { useQuery } from "convex/react";
+import { useConvexQuery } from "@/hooks/use-convex-query";
 import React from "react";
 
 const ExplorePage = () => {
-  const data = useQuery(api.events.getFeaturedEvents);
+  const data = useConvexQuery(api.events.getFeaturedEvents);
   console.log(data)
   return <div>Explore Page</div>;
 };
