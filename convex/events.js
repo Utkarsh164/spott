1057a9963c1f5fd56e2ff404bbd6f445627b723d
Utkarsh Singh Rawat <utkarsh.srawat2003@gmail.com>
2026-02-11 21:19:05 +0530

    fix

--- convex/events.js
+++ convex/events.js
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { internal } from "./_generated/api";
 
 export const createEvents = mutation({
