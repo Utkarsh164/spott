0c6f04859917f787319a1fb3020f3c80051514f7
Utkarsh Singh Rawat <utkarsh.srawat2003@gmail.com>
2026-02-11 20:08:49 +0530

    events backend query

---
+++ app/(main)/create-event/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateEvents = () => {
+  return (
+    <div>
+      i wannna cry
+    </div>
+  )
+}
+
+export default CreateEvents
---
+++ convex/events.js
@@ -0,0 +1,128 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+import { internal } from "./_generated/api";
+
+export const createEvents = mutation({
+  args: {
+    title: v.string(),
+    description: v.string(),
+    category: v.string(),
+    tags: v.array(v.string()),
+
+    startDate: v.number(),
+    endDate: v.number(),
+    timezone: v.string(),
+
+    locationType: v.union(v.literal("physical"), v.literal("online")),
+    venue: v.optional(v.string()),
+    address: v.optional(v.string()),
+    city: v.string(),
+    state: v.optional(v.string()),
+    country: v.string(),
+
+    capacity: v.number(),
+    ticketType: v.union(v.literal("free"), v.literal("paid")),
+    ticketPrice: v.optional(v.number()),
+    coverImage: v.optional(v.string()),
+    themeColor: v.optional(v.string()),
+    hasPro: v.optional(v.boolean()),
+  },
+  handler: async (ctx, args) => {
+    try {
+      const user = await ctx.runQuery(internal.users.getCurrentUser);
+      if (!args.hasPro && user.freeEventsCreated >= 1) {
+        throw new Error(
+          "Free event limit reached. Please upgrade to Pro to create more events.",
+        );
+      }
+      const defaultColor = "#1e3a8a";
+      if (!args.hasPro && args.themeColor && args.themeColor !== defaultColor) {
+        throw new Error(
+          "Custom theme colors is a Pro feature. Please upgrade to Pro.",
+        );
+      }
+      const themeColor = args.hasPro ? args.themeColor : defaultColor;
+
+      const slug = args.title
+        .toLowerCase()
+        .replace(/[^a-z0-9]+/g, "-")
+        .replace(/(^-|-$)/g, "");
+
+      const eventId = await ctx.db.insert("events", {
+        ...args,
+        themeColor,
+        slug: `${slug}-${Date.now()}`,
+        organizerId: user._id,
+        organizerName: user.name,
+        registrationCount: 0,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+      });
+      await ctx.db.patch(user._id, {
+        freeEventsCreated: user.freeEventsCreated + 1,
+      });
+
+      return eventId;
+    } catch (error) {
+      throw new Error(`Failde to create events: ${error.message}`);
+    }
+  },
+});
+
+export const getEventBySlug = query({
+  args: { slug: v.string() },
+  handler: async (ctx, args) => {
+    const event = await ctx.db
+      .query("events")
+      .withIndex("by_slug", (q) => q.eq("slug", args.slug))
+      .unique();
+    return event;
+  },
+});
+
+export const getEventByOrganizer = query({
+  handler: async (ctx) => {
+    const user = await ctx.runQuery(internal.users.getCurrentUser);
+    const event = await ctx.db
+      .query("events")
+      .withIndex("by_organizer", (q) => q.eq("organizerId", user._id))
+      .order("desc")
+      .collect();
+    return event;
+  },
+});
+
+export const deleteEvent = mutation({
+  args: { eventId: v.id("events") },
+  handler: async (ctx, args) => {
+    const user = ctx.runQuery(internal.users.getCurrentUser);
+    const event = await ctx.db.get(args.eventId);
+    if (!event) {
+      throw new Error("Event not found");
+    }
+
+    //Check id user is the orgnizer
+    if (event.organizerId !== user._id) {
+      throw new Error("You are not authorized to delete this event");
+    }
+
+    //Delete all registrations for this events
+    const registrations = await ctx.db
+      .query("registrations")
+      .withIndex("by_event", q.eq("eventId", args.eventId))
+      .collect();
+    for (const registration of registrations) {
+      await ctx.db.delete(registration._id);
+    }
+
+    await ctx.db.delete(args.eventId);
+
+    if (user.freeEventsCreated > 0) {
+      await ctx.db.patch(user._id, {
+        freeEventsCreated: user.freeEventsCreated - 1,
+      });
+    }
+
+    return { success: true };
+  },
+});
