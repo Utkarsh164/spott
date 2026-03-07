f00044b4da5e07b5fcc3c8920b531376671ea3c2
Utkarsh Singh Rawat <utkarsh.srawat2003@gmail.com>
2026-03-07 15:11:08 +0530

    f

--- app/store-user-wrapper.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useStoreUser } from "@/hooks/use-store-user";
-
-export function StoreUserWrapper({ children }) {
-  useStoreUser(); // Ensure user is stored in Convex when authenticated
-  return children;
-}
