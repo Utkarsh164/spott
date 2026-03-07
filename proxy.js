1dfe953181380565dbfea8ddd1da7c7cd7a40076
Utkarsh Singh Rawat <utkarsh.srawat2003@gmail.com>
2026-03-07 19:20:54 +0530

    proxy check part 2

--- proxy.js
+++ proxy.js
@@ -1,3 +1,32 @@
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import { NextResponse } from "next/server";
+
+// const isProtectedRoute = createRouteMatcher([
+//   "/my-events(.*)",
+//   "/create-event(.*)",
+//   "/my-tickets(.*)",
+// ]);
+
+// export default clerkMiddleware(async (auth, req) => {
+//   const { userId } = await auth();
+
+//   if (!userId && isProtectedRoute(req)) {
+//     const { redirectToSignIn } = await auth();
+//     return redirectToSignIn();
+//   }
+
+//   return NextResponse.next();
+// });
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     // Always run for API routes
+//     "/(api|trpc)(.*)",
+//   ],
+// };
+
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -8,10 +37,9 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth();
+  const { userId, redirectToSignIn } = await auth();
 
   if (!userId && isProtectedRoute(req)) {
-    const { redirectToSignIn } = await auth();
     return redirectToSignIn();
   }
 
@@ -20,9 +48,7 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
+    "/((?!_next|.*\\..*).*)",
     "/(api|trpc)(.*)",
   ],
 };
